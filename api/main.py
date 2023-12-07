75f55a30425ba706b855ae2e2a921d24fa909617
david bour <dbour.developer@gmail.com>
2023-12-06 23:41:15 -0800

    - chore: build me

--- api/main.py
+++ api/main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
 from datetime import datetime
-from opentelemetry import trace
-
-tracer = trace.get_tracer("app.api")
 
 app = FastAPI()
 
 @app.get("/")
 
 def root():
-    with tracer.start_as_current_span("root") as span:
-        the_time = str(datetime.now())
-        span.set_attribute("the_time", the_time)
-        return {"the_time": f"Today is {the_time}"}
+    return {"the_time": f"Today is {str(datetime.now())}"}
